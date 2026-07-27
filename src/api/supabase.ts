f3d2219b2d86a70e2b22651e613a2202aaf35824
jominjoon302 <jominjoon302@gmail.com>
2026-07-27 15:54:33 +0900

    Update supabase.ts

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -4,7 +4,7 @@ const supabase = axios.create({
   baseURL: import.meta.env.VITE_SUPABASE_URL,
   headers: {
     apikey: import.meta.env.VITE_SUPABASE_ANON_KEY,
-    Authorization: Bearer ${import.meta.env.VITE_SUPABASE_ANON_KEY},
+    Authorization: Bearer `${import.meta.env.VITE_SUPABASE_ANON_KEY}`,
     'Content-Type': 'application/json',
   },
 });
